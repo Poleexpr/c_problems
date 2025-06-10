b36b3471c1d5eddec32e760eabb067c0e8316c60
Poleexpr <a.c4erkasova@gmail.com>
2025-06-10 22:08:19 +0300

    feat: solve a new one

--- number_search.cpp
+++ number_search.cpp
@@ -3,37 +3,46 @@
 using namespace std;
 
 int main(){
-  int n, k;
+    int n, k;
+    cin >> n >> k;
 
-  cin >> n >> k;
+    vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+        cin >> vec[i];
+    }
 
-  vector<int> vec(n);
-  for(int i = 0; i < n; i++){
-    cin >> vec[i]; 
-  }
+    int t;
+    for (int i = 0; i < k; i++) {
+        cin >> t;
 
-  int t;
-  for(int i = 0; i < k; i++){
-    cin >> t; 
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (vec[mid] >= t) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        int first_pos = left;
 
-    int l = 0;
-    int r = n - 1;
-    bool found = false;
+        left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;
+            if (vec[mid] <= t) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        int last_pos = left;
 
-    while(l <= r){
-    int m =(r+l)/2;
-    if(t < vec[m]){
-      r = m - 1;
-    } else if(t > vec[m]){
-      l = m + 1;
-    } else if(t == vec[m]){
-      found = true;
-      break;
-    } 
-  }
+        if (vec[first_pos] != t || vec[last_pos] != t) {
+            cout << 0 << endl;
+        } else {
+            cout << first_pos + 1 << " " << last_pos + 1 << endl;
+        }
+    }
 
-  cout << (found ? "YES" : "NO") << endl;
-  }
-
-  return 0;
+    return 0;
 }
